1e1dab49c7c76a76d34fcf221de0023a1b6de97c
mustafa inaya <moose.inaya@icloud.com>
2019-04-16 19:52:29 -0700

    FINISH. Finished Adding EndPoints.

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
-const express = require('express');
-const helmet = require('helmet');
-const knex = require('knex');
-const knexConfig = require('./knexfile');
-
-const db = knex(knexConfig.development);
-
-const server = express();
-
-server.use(helmet());
-server.use(express.json());
-
-server.get('/api/cohorts', async (req, res) => {
-	try {
-		const cohorts = await db('cohorts');
-		res.status(200).json(cohorts);
-	} catch (error) {
-		res.status(500).json(error);
-	}
-});
-
-server.get('/api/cohorts/:id', async (req, res) => {
-	try {
-		const cohort = await db('cohorts').where({ id: req.params.id }).first();
-		if (!cohort) {
-			res.status(404).json({ message: 'id not found!' });
-		}
-		res.status(200).json(cohort);
-	} catch (error) {
-		res.status(500).json(error);
-	}
-});
-
+const server = require('./server');
 const port = process.env.PORT || 4444;
 
 server.listen(port, () => {
-	console.log(`\n API is Up on ${port} \n`);
+	console.log(` API is Up on ${port} `);
 });
